ee0a13086ed0578411d1624f17aaa77fa9ad2bf6
dheasrinoorseptianiz <dheasrinoorseptianiz02@gmail.com>
2025-03-23 13:11:36 +0700

    memnambahkan class SayaTubeVideo

--- tpmodul6_103022300072/Program.cs
+++ tpmodul6_103022300072/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999); 
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Dhea Sri Noor Septianiz]");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
